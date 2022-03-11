a331d5ff8bc51ba1c745c390f8a8339d5b38570c
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-03-10 21:54:35 -0300

    Banco de dados H2, test profile, JPA

---
+++ course/src/main/java/com/douglasjava/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.douglasjava.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.douglasjava.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+
+}
