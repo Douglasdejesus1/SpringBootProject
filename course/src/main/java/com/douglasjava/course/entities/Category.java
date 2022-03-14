559b17e08da000e4bbcee630062425e264d59a2f
Douglasdejesus1 <douglas.dj1242@gmail.com>
2022-03-14 15:50:19 -0300

    Entidade OrderItem. Associação muitos-para-muitos com dados extras - PARTE 1

--- course/src/main/java/com/douglasjava/course/entities/Category.java
+++ course/src/main/java/com/douglasjava/course/entities/Category.java
@@ -23,7 +23,7 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	//manyto many
+	//manyto manygit
 	@JsonIgnore
 	@ManyToMany(mappedBy="categories")
 	private Set<Product>products=new HashSet<>();
